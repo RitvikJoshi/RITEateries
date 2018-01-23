e19441d4e256c66850127cf635f39357463f0e68
RitvikJoshi <rj9336@rit.edu>
2018-01-23 13:02:26 -0500

    Data Model updates Creates schema for mongodb database

--- api/data/dataModel.js
+++ api/data/dataModel.js
@@ -1,3 +1,56 @@
 var mongoose = require('mongoose');
 
-var 
+
+ var menuItem= new mongoose.Schema({
+    category : String,
+    items : [String]
+});
+
+var review= new mongoose.Schema({
+    name : String,
+    comments : String
+});
+
+var days=new mongoose.Schema({
+    day : name,
+    openTime : Number,
+    closeTime: Number
+});
+
+
+var eateriesSchema = new mongoose.Schema({
+
+    name : {
+        type :String,
+        required: true
+    },
+    description : String,
+    menu : [menuItem],
+    daysOpen : [days],
+    location : {
+        address : String,
+        cooridnates : {
+            type: [Number],
+            index: '2dsphere'
+        }
+    },
+    reviews : [review],
+    stars : {
+        type :	Number,
+        min : 0,
+        max : 5,
+        defualt : 0
+    },
+    payment : [String],
+    speciality : [String],
+    photos : [String],
+    contact : {
+        managerName : String,
+        email : String,
+        phone : Number
+    }
+
+});
+
+
+mongoose.model('Eateries',eateriesSchema);
